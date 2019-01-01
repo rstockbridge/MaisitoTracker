1d99385fd430c5c7b1e7e5b207ba2534351e83bf
Stuart Kent <skent.dev@gmail.com>
2018-12-31 22:31:29 -0500

    Add layout for main activity so we can see it via Vysor!

--- app/src/main/java/com/github/rstockbridge/maisitotracker/MainActivity.java
+++ app/src/main/java/com/github/rstockbridge/maisitotracker/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         Log.d("maisito-tracker", "Main Activity created");
 
