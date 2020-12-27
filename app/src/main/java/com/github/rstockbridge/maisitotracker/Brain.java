fc6983017d922344768d38e03fafbdb867a27a88
Stuart Kent <skent.dev@gmail.com>
2020-12-27 14:07:48 -0500

    Make time-to-trigger configurable

--- app/src/main/java/com/github/rstockbridge/maisitotracker/Brain.java
+++ app/src/main/java/com/github/rstockbridge/maisitotracker/Brain.java
@@ -6,10 +6,11 @@ import android.util.Log;
 
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
+import static com.github.rstockbridge.maisitotracker.BuildConfig.HIGH_PRESSURE_TRIGGER_DURATION_S;
 import static com.github.rstockbridge.maisitotracker.Constants.TAG;
 import static com.github.rstockbridge.maisitotracker.Pressure.UNKNOWN;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 final class Brain {
 
@@ -50,14 +51,14 @@ final class Brain {
         if (catPresent && pendingPost == null) {
             Log.d(TAG, "Scheduling new post.");
 
-            pendingPost = executor.schedule(new Runnable() {
-                @Override
-                public void run() {
-                    Log.d(TAG, "Executing scheduled post.");
-                    pendingPost = null;
-                    listener.onShouldPost();
-                }
-            }, 5, TimeUnit.SECONDS);
+            pendingPost = executor.schedule(
+                    () -> {
+                        Log.d(TAG, "Executing scheduled post.");
+                        pendingPost = null;
+                        listener.onShouldPost();
+                    },
+                    HIGH_PRESSURE_TRIGGER_DURATION_S,
+                    SECONDS);
         } else if (!catPresent && pendingPost != null) {
             Log.d(TAG, "Canceling scheduled post.");
             pendingPost.cancel(false);
